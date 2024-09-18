26f02a55f0692f3f8ab0dc8936cbf6d1b7868ce6
Maria2721 <mashaivvas@gmail.com>
2024-09-18 14:56:53 +0300

    add example how style component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./about.module.scss";
+import about from "../../../public/images/about.png";
 
 const AboutPage = () => {
 	return (
-		<div>
+		<div className={styles.container}>
 			AboutPage
-			<div className={styles.imgContainer}>
+			<div className={[styles.imgContainer, styles.color].join(" ")}>
 				<Image
-					src="/about.png"
+					src={about}
 					alt="About Image"
 					fill
 					className={styles.img}
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import styles from "./card.module.scss";
+import about from "../../../public/images/about.png";
 
 const Card = () => {
-	return <div className={styles.container}>Card</div>;
+	return (
+		<div className={styles.container}>
+			Card
+			<div className={[styles.imgContainer, styles.color].join(" ")}>
+				<Image
+					src={about}
+					alt="About Image"
+					fill
+					className={styles.img}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
