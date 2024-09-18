8a26e8959d633f3b2b8844dc0d0470eabd85f224
Maria2721 <mashaivvas@gmail.com>
2024-09-19 02:34:03 +0300

    work with data loading

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,11 +1,16 @@
 import Card from "@/components/card/Card";
 import styles from "./events.module.scss";
+import { getEvents } from "@/lib/data";
 
-const EventsPage = () => {
+const EventsPage = async () => {
+	const events = await getEvents();
+
 	return (
 		<div className={styles.container}>
 			EventsPage
-			<Card />
+			{events.map((event) => (
+				<Card event={event} key={event.id} />
+			))}
 		</div>
 	);
 };
