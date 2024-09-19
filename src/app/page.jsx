2ea2de06baa1f25c7036267758a698370225b747
Verona4321 <sverona432@yandex.ru>
2024-09-19 09:14:46 +0400

    merge

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
 import styles from "./home.module.scss";
 
 const Home = () => {
-	return <div className="container">Homepage</div>;
+	return <div className="container">
+		<h1 className={styles.h1_title}>Крупнейший портал о всех экологичных мероприятиях Москвв</h1>
+		<div className={styles.buttons}>
+			<button>Выбрать мероприятие</button>
+			<button>Стать организатором</button>
+		</div>
+	</div>;
 };
 
 export default Home;
